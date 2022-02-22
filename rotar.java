8fdac27ff256afebd1d9205e615d65e97161ce65
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-22 17:38:48 -0300

    Create rotar.java

---
+++ rotar.java
@@ -0,0 +1,23 @@
+package misClases;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  String palabra = "contreras";
+		    System.out.println(invertirPalabra(palabra, palabra.length()-1));
+		     
+		     
+		}
+		 
+		 public static String invertirPalabra(String palabra, int longitud){
+		     
+		    if(longitud==0){
+		        return palabra.charAt(longitud)+"";
+		    }else{
+		        return palabra.charAt(longitud) + (invertirPalabra(palabra, longitud-1));
+		    }
+		     
+
+	}
+
+}
