e2924853609d6c5b033479b1fc9572838b2db2a3
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-22 21:31:50 -0300

    Update rotar.java

--- rotar.java
+++ rotar.java
@@ -1,10 +1,15 @@
 package misClases;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
+		/*Rotar un array
+		*
+		*/
 		  String palabra = "polevedroN";
-		    System.out.println(invertirPalabra(palabra, palabra.length()-1));
+		    System.out.println(invertirPalabra(palabra, palabra.length()-1)); //Mostrara en pantalla la palabra Nordevelop
 		     
 		     
 		}
